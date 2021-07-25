87b5a129020b8bc7aef3059883ae5ecfd1b75dd7
ShlokAgarwal10 <76897077+ShlokAgarwal10@users.noreply.github.com>
2021-07-25 20:10:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function home_desk(){
+    window.location = "desk.html";
+}
+function bed_desk(){
+    window.location = "bed.html";
+}
+function balcony_desk(){
+    window.location = "balcony.html";
+}
+function table_desk(){
+    window.location = "table.html";
+}
